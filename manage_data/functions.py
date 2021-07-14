da0d37f5897e8538a343549dca8ebab9fbbb4ab0
Isabela Lantero <lantero.isabela@gmail.com>
2021-07-14 15:23:09 +0200

    up to date

---
+++ manage_data/functions.py
@@ -0,0 +1,100 @@
+import regex as re
+import pandas as pd
+from pytrends.request import TrendReq
+import numpy as np
+import streamlit as st
+
+
+def limpieza(listaingredientes):
+    lista_limpia = []
+    ingrediente_limpio = ''
+    #Recorremos la lista de ingredientes que hemos cogido del dataframe en el punto interior
+    for receta in listaingredientes:
+        for ingrediente in receta:
+            #Limpiamos las cantidades que aparecen al inicio del ingrediente.
+            if re.findall("\d", ingrediente.split(' ')[0]) or re.findall("[½¼⅓]", ingrediente.split(' ')[0]):
+                ingrediente_limpio = " ".join(ingrediente.split()[1:])
+            else:
+                ingrediente_limpio = ingrediente
+            #Si hay una medida acabada en punto "g. " nos quedamos solo con lo que viene después.
+            if re.findall("\.\s", ingrediente_limpio):
+                ingrediente_limpio = " ".join(ingrediente.split('. ')[1:])
+            #Limpiamos quitando todo lo que vaya entre paréntesis.
+            ingrediente_limpio = re.sub(r'\([^())]*\)', '', ingrediente_limpio)
+            #En el caso en que haya un comentario después de una coma ", " quitamos todo lo que venga después.
+
+            #Si hay una " y " o una " o " decidimos quedarnos con el primer ingrediente porque a veces salen comentarios o más cosas
+            #Ej: canela  y si quieres sabor a cítrico, la ralladura
+            if re.findall("\sy\s|\so\s", ingrediente_limpio):            
+                ingrediente_limpio = ingrediente_limpio.split(" y ")[0]
+                ingrediente_limpio = ingrediente_limpio.split(" o ")[0]            
+            #Cuando aparecen ": " los eliminamos por no tratarse de un ingrediente principal de la receta
+            if re.findall(":", ingrediente_limpio):
+                break        
+            #si tiene ' de ' nos quedamos sólo con lo que viene después.
+            if re.findall("\sde\s", ingrediente_limpio):            
+                ingrediente_limpio = "de ".join(ingrediente_limpio.split('de ')[1:])        
+            #Si hay ingredientes vacíos, no los añadimos.
+            if ingrediente_limpio =='': break
+            lista_limpia.append(ingrediente_limpio.strip(' ').lower())
+    return lista_limpia
+
+
+
+def buscarrecetas(ingredientes, df):
+    recetas = []    
+    for i in range(len(df)):
+        cuenta = 0
+        for miingrediente in ingredientes:            
+            if miingrediente in df.loc[i, 'Ingredientes']:               
+                cuenta +=1 
+        if cuenta == len(ingredientes):            
+            recetas.append(df.loc[i, ['Nombre de la receta', 'Ingredientes', 'Link de la receta', 'Link de la imagen']])
+    #Limpiamos las recetas si tienen puntos.
+    for receta in recetas:
+        if len(receta[0].split('.'))>1:
+            receta[0] = receta[0].split('.')[0]
+    if len(recetas) > 0:
+        dfencontradas = pd.DataFrame(data=recetas, columns=['Nombre de la receta', 'Link de la receta', 'Link de la imagen'])
+        #dfencontradas['Link de la receta'] = dfencontradas.apply(convert, axis=1)
+        dfencontradas.to_csv('data/encontradas.csv', encoding ='utf-8-sig')
+        st.subheader('Con ' + ", ".join(ingredientes) + ' puedes hacer...')
+        st.header('estas fantásticas recetas:')     
+        pytrend = TrendReq()
+        for i in dfencontradas.index:
+            pytrend.build_payload(kw_list=[dfencontradas.loc[i, 'Nombre de la receta']])
+            dftrendy = pytrend.interest_by_region()
+            dftrendy = dftrendy.sort_values(by=dfencontradas.loc[i, 'Nombre de la receta'], axis=0, ascending=False)
+            dfencontradas.loc[i, 'Trendy Score'] = dftrendy.iloc[0][0]
+            if dfencontradas.loc[i, 'Trendy Score'] == 0:                
+                dfencontradas.loc[i, 'País'] = ''
+            else:
+                dfencontradas.loc[i, 'País'] = "This is trendy in " + dftrendy.index[0]                
+        #Filtramos los resultados para que sólo nos muestre aquellas recetas que tengan un Trendy Score mayor que 0.
+        #if len(dfencontradas[dfencontradas['Trendy Score']>0]) > 0:
+         #   dfencontradas = dfencontradas[dfencontradas['Trendy Score']>0].sort_values(by='Trendy Score', axis=0, ascending=False)           
+        #Sin filtrado sería:
+        dfencontradas = dfencontradas.sort_values(by='Trendy Score', axis=0, ascending=False)
+        # Reseteamos los índices y los ponemos a partir del 1 para que quede la lista mejor.
+        dfencontradas.index = np.arange(1,len(dfencontradas)+1)        
+        #st.dataframe(dfencontradas)
+        col1, col2, col3 = st.beta_columns(3)
+        st.balloons()
+        for i in range(1, len(dfencontradas)+1):           
+            if i%3==1:
+                with col1:
+                    st.header("[" + dfencontradas.loc[i, 'Nombre de la receta'] + "](" + dfencontradas.loc[i, 'Link de la receta'] + ")")
+                    st.image(dfencontradas.loc[i, 'Link de la imagen'])
+                    st.write(dfencontradas.loc[i, 'País'])
+            elif i%3==2:
+                with col2:
+                    st.header("[" + dfencontradas.loc[i, 'Nombre de la receta'] + "](" + dfencontradas.loc[i, 'Link de la receta'] + ")")
+                    st.image(dfencontradas.loc[i, 'Link de la imagen'])
+                    st.write(dfencontradas.loc[i, 'País'])
+            else:
+                with col3:
+                    st.header("[" + dfencontradas.loc[i, 'Nombre de la receta'] + "](" + dfencontradas.loc[i, 'Link de la receta'] + ")")
+                    st.image(dfencontradas.loc[i, 'Link de la imagen'])
+                    st.write(dfencontradas.loc[i, 'País'])
+    else:
+        st.write('Lo sentimos mucho, no hemos encontrado ninguna receta con estos ingredientes!!')
---
+++ mystreamlit22.py
@@ -0,0 +1,49 @@
+import string
+import regex as re
+import numpy as np
+import pandas as pd
+from pytrends.request import TrendReq
+import streamlit as st
+import manage_data.functions as md
+
+st.sidebar.title('Selecciona tus ingredientes favoritos!')
+df = pd.read_csv('data/recetasfaciles.csv')
+dfingredientes = pd.read_csv('data/ingredientes.csv')
+dfmilista = pd.read_csv('data/milista.csv')
+listaingredientes = dfingredientes.Ingrediente.tolist()
+misletras = list(string.ascii_uppercase)
+primeraletra = st.sidebar.selectbox('Selecciona la primera letra del ingrediente que quieras buscar:', misletras)
+
+ingredientesconesaletra = []
+for ingrediente in listaingredientes:
+    if re.findall(rf"^{primeraletra.lower()}+", str(ingrediente)):
+        ingredientesconesaletra.append(ingrediente)
+
+youringredients = st.sidebar.selectbox('Elige ahora el ingrediente o ingredientes que empiezan con...' + primeraletra, ingredientesconesaletra) 
+
+
+def convert(row):
+    return '[link]({})'.format(row['Link de la receta'],  row.name)
+
+    
+if st.sidebar.button('Añadir ingrediente a mi lista'):
+    #Guardamos los ingredientes que vamos añadiendo en un dataframe.
+    dfmilista.loc[len(dfmilista), ['Mi lista']] = youringredients        
+    dfmilista.to_csv('data/milista.csv')
+    #Rehacemos el dataframe para que solo contenga ingredientes diferentes, que no haya repetidos.
+    dfmilista = pd.DataFrame(data=dfmilista['Mi lista'].unique(), columns=['Mi lista'])
+    #Le pedimos que empiece con índice 1 para que al mostrar la lista quede mejor.
+    dfmilista.index = np.arange(1,len(dfmilista)+1)
+    dfmilista.to_csv('data/milista.csv')
+    #Mostramos la lista de ingredientes que llevamos seleccionados en streamlit.        
+    #Botón para buscar las recetas con los ingredientes de la lista.
+    st.write(dfmilista)
+
+if st.sidebar.button('Buscar recetas con estos ingredientes'):
+    dfencontradas = md.buscarrecetas(dfmilista['Mi lista'].unique(), df)
+
+    
+if st.sidebar.button('Resetear mi lista de ingredientes'):
+    dfmilista.drop(columns='Mi lista', inplace = True)
+    dfmilista = pd.DataFrame(columns=['Mi lista'])
+    dfmilista.to_csv('data/milista.csv')
